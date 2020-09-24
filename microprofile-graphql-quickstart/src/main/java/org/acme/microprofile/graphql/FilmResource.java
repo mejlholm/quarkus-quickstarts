475ded47a368b8a442886f62e02724068fbfefbe
Phillip Kruger <phillip.kruger@gmail.com>
2020-09-24 12:10:35 +0200

    Added films Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- microprofile-graphql-quickstart/src/main/java/org/acme/microprofile/graphql/FilmResource.java
+++ microprofile-graphql-quickstart/src/main/java/org/acme/microprofile/graphql/FilmResource.java
@@ -7,46 +7,51 @@ import org.eclipse.microprofile.graphql.Name;
 import org.eclipse.microprofile.graphql.Query;
 import org.eclipse.microprofile.graphql.Source;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.List;
+import org.eclipse.microprofile.graphql.GraphQLApi;
 
-@ApplicationScoped
+@GraphQLApi
 public class FilmResource {
 
     @Inject
     GalaxyService service;
 
-    @Query("allFilms")
+    
+    @Query("Films")
+    public Films getFilms(){
+        return new Films();
+    }
+    
     @Description("Get all Films from a galaxy far far away")
-    public List<Film> getAllFilms() {
+    public List<Film> getAllFilms(@Source Films films) {
         return service.getAllFilms();
     }
 
-    @Query("getFilm")
-    @Description("Get a Films from a galaxy far far away")
-    public Film getFilm(@Name("filmId") int id) {
-        return service.getFilm(id);
-    }
-
-    public List<Hero> heroes(@Source Film film) {
-        return service.getHeroesByFilm(film);
-    }
-
-    @Mutation("createHero")
-    public Hero createHero(Hero hero) {
-        service.addHero(hero);
-        return hero;
-    }
-
-    @Mutation("deleteHero")
-    public Hero deleteHero(int id) {
-        return service.deleteHero(id);
-    }
-
-    @Query("getHeroesWithSurname")
-    public List<Hero> getHeroesWithSurname(@DefaultValue("Skywalker") String surname) {
-        return service.getHeroesBySurname(surname);
-    }
+//    @Query("getFilm")
+//    @Description("Get a Films from a galaxy far far away")
+//    public Film getFilm(@Name("filmId") int id) {
+//        return service.getFilm(id);
+//    }
+//
+//    public List<Hero> heroes(@Source Film film) {
+//        return service.getHeroesByFilm(film);
+//    }
+//
+//    @Mutation("createHero")
+//    public Hero createHero(Hero hero) {
+//        service.addHero(hero);
+//        return hero;
+//    }
+//
+//    @Mutation("deleteHero")
+//    public Hero deleteHero(int id) {
+//        return service.deleteHero(id);
+//    }
+//
+//    @Query("getHeroesWithSurname")
+//    public List<Hero> getHeroesWithSurname(@DefaultValue("Skywalker") String surname) {
+//        return service.getHeroesBySurname(surname);
+//    }
 
 }
